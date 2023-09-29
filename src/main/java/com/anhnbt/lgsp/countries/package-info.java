511a7fe9bb1868eeecee24fd4ef8eccfec680576
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2023-09-29 13:34:11 +0700

    Xóa class cũ

--- src/main/java/com/anhnbt/lgsp/countries/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.anhnbt.lgsp.countries;
